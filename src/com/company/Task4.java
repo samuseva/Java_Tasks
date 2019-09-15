96adb16f11b631e7a49ca258630b243cd2392a99
samuseva <samusevalex@mail.ru>
2019-09-16 01:01:05 +0300

    Update Task4.java

--- src/com/company/Task4.java
+++ src/com/company/Task4.java
@@ -4,11 +4,13 @@ import java.util.Random;
 
 public class Task4 {
     void start() {
+        
+        int a1, a1n, a2, a2n, arrLen=10, i=-2;
         Random random = new Random();
-        int arr1[] = new int[10];
-        int arr2[] = new int[10];
-        int a1, a1n, a2, a2n, i=-2;
-        while((i+=2)<10) {
+        int arr1[] = new int[arrLen];
+        int arr2[] = new int[arrLen];
+        
+        while((i+=2)<arrLen) {              //Increment +2 until index < arrLen
             a1 = arr1[i] = random.nextInt(9);
             a1n = arr1[i+1] = random.nextInt(9);
             a2 = arr2[i] = random.nextInt(9);
