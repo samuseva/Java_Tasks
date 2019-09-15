44be67acfedc9920c484366a741b7a5edd4c38bb
samuseva <samusevalex@mail.ru>
2019-09-15 23:34:10 +0300

    Update Task3.java

--- src/com/company/Task3.java
+++ src/com/company/Task3.java
@@ -3,30 +3,31 @@ package com.company;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Task3 {
-    void func(AtomicInteger i) {
-        i.set(2000);
-        System.out.println("AtomicInteger = " + i);
-    }
-
+    
     void func(int i) {
         i = 20;
         System.out.println("int = " + i);
     }
-
+    
+    void func(AtomicInteger i) {
+        i.set(2000);
+        System.out.println("AtomicInteger = " + i);
+    }
+    
     void start() {
-        int iVar = 10;
+        int intVar = 10;
         AtomicInteger atomicVar = new AtomicInteger(1000);
 
         System.out.println("Before Function");
-        System.out.println("int = " + iVar);
+        System.out.println("int = " + intVar);
         System.out.println("AtomicInteger = " + atomicVar);
 
         System.out.println("In Function");
-        func(iVar);
+        func(intVar);
         func(atomicVar);
 
         System.out.println("After Function");
-        System.out.println("int = " + iVar);
+        System.out.println("int = " + intVar);
         System.out.println("AtomicInteger = " + atomicVar);
     }
 }
