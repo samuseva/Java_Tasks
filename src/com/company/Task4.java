e7efea94b09280e44d56a395282426b0aba7d53f
samuseva <samusevalex@mail.ru>
2019-09-16 00:53:21 +0300

    Update Task4.java

--- src/com/company/Task4.java
+++ src/com/company/Task4.java
@@ -7,15 +7,15 @@ public class Task4 {
         Random random = new Random();
         int arr1[] = new int[10];
         int arr2[] = new int[10];
-        int i=0;
-        do {
-            int a1 = arr1[i] = random.nextInt(9);
-            int a1n = arr1[i+1] = random.nextInt(9);
-            int a2 = arr2[i] = random.nextInt(9);
-            int a2n = arr2[i+1] = random.nextInt(9);
+        int a1, a1n, a2, a2n, i=-2;
+        while((i+=2)<10) {
+            a1 = arr1[i] = random.nextInt(9);
+            a1n = arr1[i+1] = random.nextInt(9);
+            a2 = arr2[i] = random.nextInt(9);
+            a2n = arr2[i+1] = random.nextInt(9);
             System.out.println(a1==a2 ? a1+" = "+a2 : a1+" <> "+a2);
             System.out.printf("%d %s %d\n", a1n, a1n==a2n ? "=" : "<>", a2n);
-        } while((i+=2)<10);
+        }
 
     }
 }
