4c5430f4a330ba435a43e33422472d3f7bcd028b
samuseva <samusevalex@mail.ru>
2019-11-05 10:41:44 +0300

    Update Task9.java

--- src/com/company/Task9/Task9.java
+++ src/com/company/Task9/Task9.java
@@ -4,5 +4,6 @@ public class Task9 {
     public Task9(){
         TimeOfDay tod = TimeOfDay.Morning;
         System.out.println(tod);
+        System.out.println(Arrays.asList(TimeOfDaySystem.values()));
     }
 }
